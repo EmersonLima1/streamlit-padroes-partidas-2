93b01ffb3d03f941258174fbc29762f6a20ae4a7
EmersonLima1 <74200172+EmersonLima1@users.noreply.github.com>
2023-06-12 07:36:23 -0300

    Update partidas_padroes_2.py

--- partidas_padroes_2.py
+++ partidas_padroes_2.py
@@ -4,6 +4,10 @@ import streamlit as st
 from io import BytesIO
 from openpyxl import load_workbook
 
+st.set_page_config(page_title="InPES Futebol Virtual", page_icon=":soccer:")
+
+st.title('**Resultados do Futebol Virtual**')
+
 # Perguntas ao usuário
 primeiro_tempo1 = st.selectbox("Qual o resultado do primeiro tempo?", ['0x0','0x1','0x2','0x3','0x4','0x5','1x0','1x1','1x2','1x3','1x4','1x5','2x1','2x2','2x3','2x4','2x5','3x1','3x2','3x3','3x4','3x5','4x1','4x2','4x3','4x4','4x5','5x1','5x2','5x3','5x4','5x5'])
 tempo_final1 = st.selectbox("Qual o resultado do tempo final?", ['0x0','0x1','0x2','0x3','0x4','0x5','1x0','1x1','1x2','1x3','1x4','1x5','2x1','2x2','2x3','2x4','2x5','3x1','3x2','3x3','3x4','3x5','4x1','4x2','4x3','4x4','4x5','5x1','5x2','5x3','5x4','5x5'])
